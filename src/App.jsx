395c807071c7fbcab5cffafb07b5f8329df6e4bc
Fiyaz Ahmed Totad <fiyazahmedtotad@fiyazs-mbp.home>
2025-04-14 15:35:58 -0400

    Test /app route with placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const InnerApp = () => {
   <Route path="/analytics-reporting" element={<AnalyticsReporting />} />
   <Route path="/contacts-manager" element={<ContactsManager />} />
   <Route path="/design-sequence" element={<DesignSequence />} />
-  <Route path="/app" element={<DashboardLayout />} />
+  <Route path="/app" element={<div style={{ padding: 40 }}><h1>Hello from /app!</h1></div>} />
 </Routes>
   );
 };
